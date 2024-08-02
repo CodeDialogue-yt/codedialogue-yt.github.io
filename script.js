00a28986bf996d4b8d8be619b15aad6de2ef284e
CodeDialogue <code.dialogue.official@gmail.com>
2024-08-02 11:31:01 +0200

    Centering

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
-// Create image elements
-var python = document.createElement("img");
-python.src = "python.png";
-python.alt = "Python";
-var js = document.createElement("img");
-js.src = "javascript.png";
-js.alt = "JavaScript";
+        // Create image elements
+        var python = document.createElement("img");
+        python.src = "python.png";
+        python.alt = "Python";
+        
+        var js = document.createElement("img");
+        js.src = "javascript.png";
+        js.alt = "JavaScript";
+        
+        var java = document.createElement("img");
+        java.src = "java.png";
+        java.alt = "Java";
+        
+        var c = document.createElement("img");
+        c.src = "c.png";
+        c.alt = "C";
+        
+        var cpp = document.createElement("img");
+        cpp.src = "cpp.png";
+        cpp.alt = "C++";
+        
+        var cs = document.createElement("img");
+        cs.src = "cs.png";
+        cs.alt = "C#";
 
-var java = document.createElement("img");
-java.src = "java.png";
-java.alt = "Java";
+        // Dictionary with programming languages
+        var languages = {
+            "Python": python,
+            "JavaScript": js,
+            "Java": java,
+            "C": c,
+            "C++": cpp,
+            "C#": cs
+        };
 
-// Create image elements
-var c = document.createElement("img");
-c.src = "c.png";
-c.alt = "C";
+        // Function to pick a random element from the dictionary
+        function pickRandomLanguage() {
+            var keys = Object.keys(languages);
+            var randomKey = keys[Math.floor(Math.random() * keys.length)];
+            var randomElement = languages[randomKey];
 
-var cpp = document.createElement("img");
-cpp.src = "cpp.png";
-cpp.alt = "C++";
+            // Clear previous output and add the new image
+            var container = document.getElementById("imageContainer");
+            container.innerHTML = ""; // Clear previous output
+            container.appendChild(randomElement); // Append the image
+        }
 
-var cs = document.createElement("img");
-cs.src = "cs.png";
-cs.alt = "C#";
-
-
-// Dictionary with programming languages
-var languages = {
-    "Python": python,
-    "JavaScript": js,
-    "Java": java,
-    "C": c,
-    "C++": cpp,
-    "C#": cs
-};
-
-// Function to pick a random element from the dictionary
-function pickRandomLanguage() {
-    var keys = Object.keys(languages);
-    var randomKey = keys[Math.floor(Math.random() * keys.length)];
-    var randomElement = languages[randomKey];
-
-    // Output key and value
-    document.body.innerHTML = ""; // Clear previous output
-    document.body.appendChild(randomElement); // Append the image
-}
-
-// Call the function to pick and display a random language
-pickRandomLanguage();
+        // Call the function to pick and display a random language
+        pickRandomLanguage();
